93b7de851a0152476501858f2dd41fc015168724
L00170565 <L00170565@student.lyit.ie>
2021-11-01 06:46:26 -0700

    Merge branch 'master' of git@github.com:L00170565/MavenProj.git

---
+++ MavenProj/src/main/java/mvnSimpleProj/demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mvnSimpleProj;
+
+/**
+ * @author dev
+ *
+ */
+public class demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
