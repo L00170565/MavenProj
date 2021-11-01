b913edd835585ad315841f75fbb4e8e482c4e2d2
L00170565 <L00170565@student.lyit.ie>
2021-11-01 01:57:10 -0700

    first commit

---
+++ MavenProj/src/main/java/mvnSimpleProj/App.java
@@ -0,0 +1,20 @@
+/**
+ * Simple Java Application 
+ */
+package mvnSimpleProj;
+
+/**
+ * @author Panagiotis Drakos
+ * Student Id: L00170565 
+ */
+public class App {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("This is a simple Java application");
+	}
+
+}
