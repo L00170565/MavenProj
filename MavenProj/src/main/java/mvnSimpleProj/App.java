319921d0c7aea84c2f8e1b656f444125f6027734
L00170565 <L00170565@student.lyit.ie>
2021-11-01 06:42:00 -0700

    Added Hello there msg

--- MavenProj/src/main/java/mvnSimpleProj/App.java
+++ MavenProj/src/main/java/mvnSimpleProj/App.java
@@ -15,6 +15,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("This is a simple Java application test");
+        System.out.println("Hello there");
 	}
 
 }
